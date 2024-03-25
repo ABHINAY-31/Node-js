d52efe9df2d607e9185e15a17bd1ea472393bf6d
ABHINAY-31 <rsingh9766@gmail.com>
2024-03-26 00:34:59 +0530

    http

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-// import fs from 'fs';
+const http = require('http');
+// const data = {name : "abhinay"};
 const fs = require('fs');
-// const data = fs.readFileSync('demo.txt', 'utf-8');
 
-const express = require('express');
+const value = fs.readFileSync('index.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const product = data.products[0];
+const server = http.createServer((req, res) => {
+    console.log("server Started");
+    console.log(req.url);
+    if(req.url.startsWith('/product')){
+        console.log(req.url);
+        console.log(req.url.split('/'));
+        let id = req.url.split('/')[2];
+        const prd = data.products.find(p => p.id == id);
+        console.log(prd);
+        res.setHeader('Content-Type', 'text/html'); 
+            //     console.log(product);
+        let index = value.replace('**val1**', prd.title).replace('**val2**', prd.stock).replace('**val3**', prd.rating);
+        res.end(index);
+        
+    }
+    // switch(req.url){
+    //     case '/':
+    //         res.setHeader('Content-Type', 'text/html'); 
+    //         console.log(product);
+    //         let index = value.replace('**val1**', product.title);
+    //         res.end(index);
+    //         break;
+    //     case '/api':
+    //         res.setHeader('Content-Type', 'application/json'); 
+    //         res.end(JSON.stringify(data));
+    //         break;
+    //     default:
+    //         res.writeHead(404);
+    //         res.end();
+    // }
+    // res.setHeader('dummy', 'Abhinay Singh');
+    // // res.setHeader('content-Type', 'application/json'); 
+    // res.end(value);
+})
 
-const server = express();
-server.listen(3001);
-
-// console.log("demo");
-
-
-
-
-
-
-
-
-
-
-
-
-
-fs.readFile('demo.txt', 'utf-8', (error, data) => {
-    // result will be in the callback (readFile)
-    // readfile have two parameter (error and result)
-    console.log(data);
-});
-
-console.log("done the task");
-// console.log(data);
+server.listen(8080); // bind our server to this port
